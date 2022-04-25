d1b3fbd1c0660227b51cfbf40122c00f52ab3849
Фаддей Гусаров <gusarovfg@gmail.com>
2022-04-25 14:41:38 +0400

    feat: Реализована гиперссылка в agreeLabel для перехода на источник пользовательских соглашений

---
+++ GreenWallet/GreenWallet/Extensions/CustomClasses.swift
@@ -0,0 +1,8 @@
+//
+//  CustomClasses.swift
+//  GreenWallet
+//
+//  Created by Фаддей Гусаров on 25.04.2022.
+//
+
+import Foundation
--- GreenWallet/GreenWallet/TermsOfUseViewController.swift
+++ GreenWallet/GreenWallet/TermsOfUseViewController.swift
@@ -8,22 +8,63 @@
 import UIKit
 
 class TermsOfUseViewController: UIViewController {
-
+    
+    @IBOutlet weak var continueButton: UIButton!
+    @IBOutlet weak var checkBoxButton: UIButton!
+    @IBOutlet weak var agreeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.continueButton.isEnabled = false
+        self.continueButton.backgroundColor = #colorLiteral(red: 0.6106664538, green: 0.6106664538, blue: 0.6106664538, alpha: 1)
+        self.continueButton.tintColor = .black
+        
+        
 
-        // Do any additional setup after loading the view.
+        let text = "Я соглашаюсь с условиями пользования"
+        self.agreeLabel.text = text
+        self.agreeLabel.textColor =  UIColor.white
+        let underlineAttriString = NSMutableAttributedString(string: text)
+        let range1 = (text as NSString).range(of: "условиями пользования")
+             underlineAttriString.addAttribute(NSAttributedString.Key.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: range1)
+        self.agreeLabel.attributedText = underlineAttriString
+        self.agreeLabel.isUserInteractionEnabled = true
+        self.agreeLabel.addGestureRecognizer(UITapGestureRecognizer(target:self, action: #selector(tapLabel(gesture:))))
     }
     
+    @objc func tapLabel(gesture: UIGestureRecognizer) {
+        let termsRange = (text as NSString).range(of: "Terms & Conditions")
+        // comment for now
+        //let privacyRange = (text as NSString).range(of: "Privacy Policy")
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if gesture.didTapAttributedTextInLabel(label: lblTerms, inRange: termsRange) {
+            print("Tapped terms")
+        } else if gesture.didTapAttributedTextInLabel(label: lblTerms, inRange: privacyRange) {
+            print("Tapped privacy")
+        } else {
+            print("Tapped none")
+        }
     }
-    */
-
+    
+    @IBAction func tappedOnCheckboxButton(_ sender: UIButton){
+        if sender.imageView?.image != UIImage(systemName: "checkmark.square.fill") {
+            sender.setImage(UIImage(systemName: "checkmark.square.fill"), for: .normal)
+            sender.tintColor = #colorLiteral(red: 0.2681596875, green: 0.717217505, blue: 0.4235975146, alpha: 1)
+            sender.backgroundColor = .white
+            sender.imageView?.layer.cornerRadius = 5
+            
+            self.continueButton.backgroundColor = #colorLiteral(red: 0.2681596875, green: 0.717217505, blue: 0.4235975146, alpha: 1)
+            self.continueButton.isEnabled = true
+        } else {
+            sender.setImage(UIImage(systemName: "squareshape.fill"), for: .normal)
+            sender.imageView?.layer.cornerRadius = 5
+            sender.tintColor = .white
+            
+            self.continueButton.backgroundColor = #colorLiteral(red: 0.6106664538, green: 0.6106664538, blue: 0.6106664538, alpha: 1)
+            self.continueButton.isEnabled = false
+        }
+        
+    }
+    
 }
