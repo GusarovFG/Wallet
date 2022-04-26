7a09060d849d75cd6e5eb038246d3b4cd1f757c9
Фаддей Гусаров <gusarovfg@gmail.com>
2022-04-26 14:40:38 +0400

    qwe

---
+++ GreenWallet/GreenWallet/CutomCells/BalanceTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BalanceTableViewCell.swift
+//  GreenWallet
+//
+//  Created by Фаддей Гусаров on 26.04.2022.
+//
+
+import UIKit
+
+class BalanceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
